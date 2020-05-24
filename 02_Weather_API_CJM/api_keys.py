bfa1f6b6036ce73a7bf4b1ed4242781ddeda3698
unknown <cmikus6@gmail.com>
2020-05-23 23:58:56 -0400

    Uploading HW

---
+++ 02_Weather_API_CJM/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = '8e2dedb2f91bed5822ebbee55664f5b6'
+
+# Google API Key
+g_key = 'AIzaSyD3HSuZPQIx6QwpXXJdZpBlad9A8VnXB-E'
---
+++ config.py
@@ -0,0 +1,4 @@
+consumer_key = "AYX0e2brBSkIt2FfIDq9iF8pe"
+consumer_secret = "dKBDibhLOnv4ZKum1PwrxTlRYAl0StATcCid94UAutuONgKObu"
+access_token = "1255313279670460417-Tov3jZJu6e08YLmjUixpuA5rsWHBNY"
+access_token_secret = "etZBDTXptehwFM5mpvE6Ra7D98gkYhliaqmzhrB0iTgcY"
